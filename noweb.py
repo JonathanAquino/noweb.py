1da4ed2d767f2cc7f7a8dde74b142378e2ac9d29
Jonathan Aquino <jonathan.aquino@gmail.com>
2010-04-03 13:33:48 -0700

    Initial commit

---
+++ noweb.py
@@ -0,0 +1,43 @@
+#! /usr/local/bin/python
+
+#
+# noweb.py
+# By Jonathan Aquino (jonathan.aquino@gmail.com)
+#
+# This program extracts code from a literate programming document in "noweb" format.
+# It was generated from noweb.py.txt, itself a literate programming document.
+# For more information, including the original source code and documentation,
+# see ...
+#
+
+import sys, re
+filename = sys.argv[-1]
+outputChunkName = sys.argv[-2][2:]
+file = open(filename)
+chunkName = None
+chunks = {}
+OPEN = "<<"
+CLOSE = ">>"
+for line in file:
+    match = re.match(OPEN + "([^>]+)" + CLOSE + "=", line)
+    if match:
+        chunkName = match.group(1)
+        chunks[chunkName] = []
+    else:
+        match = re.match("@", line)
+        if match:
+            chunkName = None
+        elif chunkName:
+            chunks[chunkName].append(line)
+def expand(chunkName, indent):
+    chunkLines = chunks[chunkName]
+    expandedChunkLines = []
+    for line in chunkLines:
+        match = re.match("(\s*)" + OPEN + "([^>]+)" + CLOSE + "\s*$", line)
+        if match:
+            expandedChunkLines.extend(expand(match.group(2), indent + match.group(1)))
+        else:
+            expandedChunkLines.append(indent + line)
+    return expandedChunkLines
+for line in expand(outputChunkName, ""):
+    print line,
